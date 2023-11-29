4366197f2b1407f507c95f329d69d9447974856e
lbccjyx <1033070036@qq.com>
2023-11-29 18:42:57 +0800

    requirement

---
+++ text/text.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class Test(unittest.TestCase):
+    def init(self) -> bool:
+        return True
+
+    def test_a(self):
+        import os
+        return
